250f6a841fdd14686a006b3f3ef944a422cb42d0
sagar <sagarlama826@gmail.com>
2019-07-08 19:13:55 +0545

    Information Retrieved from Database

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php include("header.php"); ?>
 <?php include("nav.php"); ?>
 <div class="container">
+<?php
+require("./includes/dbh.inc.php");
+$sql = "select * from books";
 
+$result = $conn->query($sql);
+if ($result->num_rows>0){
+		while($row = $result->fetch_assoc()){
+				echo "id: ". $row['id']."</br>";
+				echo "Title: ". $row['title']."</br>";
+				echo "Description: ". $row['description']."</br>";
+		}
+}
+?>
 
 
 </div>
