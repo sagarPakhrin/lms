83ee3a9063ae45dfecd14b46e756b310182ee69a
sagar <sagarlama826@gmail.com>
2019-07-08 21:00:26 +0545

    book detail page aded

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include("header.php"); ?>
 <?php include("nav.php"); ?>
 <div class="container">
+<div class="row">
 <?php
 require("./includes/dbh.inc.php");
 $sql = "select * from books";
@@ -8,14 +9,24 @@ $sql = "select * from books";
 $result = $conn->query($sql);
 if ($result->num_rows>0){
 		while($row = $result->fetch_assoc()){
-				echo "id: ". $row['id']."</br>";
-				echo "Title: ". $row['title']."</br>";
-				echo "Description: ". $row['description']."</br>";
+		echo '				<div class="col s12 m4">';
+		echo '				<div class="card">';
+		echo '				<div class="card-image">';
+		echo '				<img src="./uploads/default.jpg">';
+		echo '				<span class="card-title">'.$row['title'].'</span>';
+		echo '				</div>';
+		echo '				<div class="card-content">';
+		echo '				<p>'.$row['description'].'</p>';
+		echo '				</div>';
+		echo '				<div class="card-action">';
+		echo '				<a href="./bookdetail.php?id='.$row['id'].'">Burrow This Book</a>';
+		echo '				</div>';
+		echo '				</div>';
+		echo '				</div>';
 		}
 }
 ?>
-
-
+</div>
 </div>
 
 <?php include("footer.php"); ?>
